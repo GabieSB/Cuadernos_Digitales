00f50b782432a93b7332ad04a54650cc05a70a3b
DanielGCh1 <dgchaarturo@gmail.com>
2019-12-23 03:43:33 -0600

    Añidiendo Bitacora y Form Usuario

---
+++ CuadernosDigitales/CuadernosDigitales/UNA/CuadernosDigitales/Clases/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CuadernosDigitales
+{
+    public class Usuario
+    {
+        public string Nombre
+        {
+            get;
+            set;
+        }
+
+        public string Contraseña
+        {
+            get;
+            set;
+        }
+
+        public int Identificador
+        {
+            get;
+            set;
+        }
+    }
+}
